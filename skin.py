45ccedc970e70ff4068978c24bd6860c18814540
Shreya-b510 <yuvasamajsewarautaht@gmail.com>
2025-12-01 22:48:26 +0545

    Initial commit for 700MB project

---
+++ skin.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0ff74b0d9583201220f555e4aba5c670e428e01c5894a202fbf650f83dc78267
+size 7742
---
+++ train.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:77b8c43882aab6a434739a77d3fab7b53ba9e5dc9788580a305e0321aa6fd8dc
+size 5831
